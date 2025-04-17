06c80905fde737d709a03074a502feffe2743e88
Macit Simsek <macitsimsek12@hotmail.com>
2025-04-17 19:30:45 +0100

    feat: refactor Toaster component to enhance toast rendering and layout

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -29,7 +29,22 @@ export function Toaster() {
           </Toast>
         )
       })}
-      <ToastViewport />
+      <div className="fixed top-0 left-0 right-0 z-[100] flex flex-col-reverse items-end p-4 space-y-2 max-h-60 overflow-y-auto sm:items-end sm:right-0 sm:left-auto sm:top-0 sm:max-w-[420px]">
+        {toasts.map(function ({ id, title, description, action, ...props }) {
+          return (
+            <Toast key={id} {...props}>
+              <div className="grid gap-1">
+                {title && <ToastTitle>{title}</ToastTitle>}
+                {description && (
+                  <ToastDescription>{description}</ToastDescription>
+                )}
+              </div>
+              {action}
+              <ToastClose />
+            </Toast>
+          )
+        })}
+      </div>
     </ToastProvider>
   )
 }
